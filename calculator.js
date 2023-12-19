cce09143c4755b04bd7239bfc3671bac5eb0e8a4
suraj verma <surajuniverse07@gmail.com>
2023-12-19 09:33:03 +0530

    usual commit

--- calculator.js
+++ calculator.js
@@ -1,1 +1,7 @@
-console.log("hello calcy :");
+const numbers=document.querySelectorAll('numbers');
+const delete_number=document.querySelector('delete-number');
+const all_clear_number=document.querySelector('all-clear');
+const sub_screan_1=document.querySelector('sub-screan-1');
+const sub_screan_2=document.querySelector('sub-screan-2');
+const number_operation=document.querySelectorAll('number-operation');
+const equal_number=document.querySelector('equal-number');
